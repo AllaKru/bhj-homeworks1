c1de64e4eb3aa3910f4da1bd4cf3748c2f61b43d
Alla Kruglova <renata-l@yandex.ru>
2020-08-10 20:07:56 +0300

    New mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,23 +1,29 @@
-let countHole = parseInt(document.getElementById("dead").textContent);
-let countnoHole = parseInt(document.getElementById("lost").textContent);
-
+let countHole = document.getElementById("dead");
+let countnoHole = document.getElementById("lost");
+let countparseHole = parseInt(countHole.textContent);
+let countparsenoHole = parseInt(countnoHole.textContent);
 getHole = (index) => {
   return document.getElementById(`hole${index}`);
 };
 
-for (let index = 1; index <= 10; index++) {
+for (let index = 1; index <= 9; index++) {
   getHole(index).onclick = function () {
-    if (hole.className.includes("hole_has-mole")) {
-      countHole++;
-    } else countnoHole++;
+    countHole.textContent = countparseHole;
+    countnoHole.textContent = countparsenoHole;
+    if (getHole(index).className.includes("hole_has-mole")) {
+      countparseHole++;
+    } else {
+      countparsenoHole++;
+    }
+
+    if (countparsenoHole == 5) {
+      alert("Вы проиграли!");
+      countparseHole = 0;
+      countparsenoHole = 0;
+    } else if (countparseHole == 10) {
+      alert("Вы выиграли!");
+      countparseHole = 0;
+      countparsenoHole = 0;
+    }
   };
-}
-if (countnoHole == 5) {
-  countHole = 0;
-  countnoHole = 0;
-  alert("Вы проиграли!");
-} else if (countHole == 10) {
-  countHole = 0;
-  countnoHole = 0;
-  alert("Вы выиграли!");
 }
